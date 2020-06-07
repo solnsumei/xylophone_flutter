df997df3f91358af1633a6a29bdb35df98ef579a
solnsumei <solnsumei@gmail.com>
2020-06-07 10:06:22 +0100

    Add xylophone functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(MyApp());
 
-class XylophoneApp extends StatelessWidget {
-  @override
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        backgroundColor: Colors.black,
+        body: XylophoneApp(),
+      ),
+    );
+  }
+}
+
+
+
+class XylophoneApp extends StatelessWidget {
+
+  Widget circleDot() {
+    return CircleAvatar(
+      radius: 5.0,
+      backgroundColor: Colors.black,
+    );
+  }
+
+  Widget buildTone({ Color color, int soundNumber, double width}) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 2.0),
+        width: width ?? double.infinity,
+        child: FlatButton(
+          color: color,
+          shape: StadiumBorder(),
+          onPressed: () {
+            final player = AudioCache();
+            player.play('note$soundNumber.wav');
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              circleDot(),
+              circleDot(),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(),
+          buildTone(
+            width: appWidth * 0.95,
+            color: Colors.red,
+            soundNumber: 1,
+          ),
+          buildTone(
+            width: appWidth * 0.9,
+            color: Colors.orange,
+            soundNumber: 2,
+          ),
+          buildTone(
+              width: appWidth * 0.85,
+              color: Colors.yellow,
+              soundNumber: 3
+          ),
+          buildTone(
+            width: appWidth * 0.8,
+            color: Colors.green,
+            soundNumber: 4,
+          ),
+          buildTone(
+            width: appWidth * 0.75,
+            color: Colors.teal,
+            soundNumber: 5,
+          ),
+          buildTone(
+            width: appWidth * 0.7,
+            color: Colors.blue,
+            soundNumber: 6,
+          ),
+          buildTone(
+            width: appWidth * 0.65,
+            color: Colors.purple,
+            soundNumber: 7,
+          ),
+        ],
       ),
     );
   }
